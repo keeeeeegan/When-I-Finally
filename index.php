ac6a13f456ec6b8f43121e5ee7d7c90424a95be7
Keegan Berry <keeganberry@gmail.com>
2012-04-23 15:47:21 -0400

    added profile page

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (isset($_SESSION) && isset($_SESSION["status"]) && $_SESSION["status"] == "ve
 </div>
 
 <div class="sign_in">
-	<a href="twitteroauth/redirect.php" class="twitter_button">Sign in using your Twitter account</a>
+	<a href="twitteroauth/redirect.php" class="button twitter_button">Sign in using your Twitter account</a>
 </div>
 
 <footer>
--- profile.php
+++ profile.php
@@ -57,17 +57,13 @@ $local_user = getLocalUser($content->id, $content->name);
 
 <div class="description">
 
-	<h3>You are logged in!</h3>
+	<h3><?php echo $local_user["nicename"]; ?>'s Motivations</h3>
 
-	<p>Donec quis dui eget arcu fringilla accumsan et a ligula. Sed quis lectus ac neque fermentum rutrum sagittis quis dolor. Aenean sagittis sapien non sapien porta vehicula pulvinar augue aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id ligula eu purus adipiscing pharetra. Vivamus interdum ornare felis, eu sollicitudin massa scelerisque vitae. Nunc molestie, ipsum ut lacinia tempus, neque lacus interdum enim, quis mattis diam ipsum id metus. Maecenas dignissim erat ac sem gravida malesuada. Donec tempor semper odio a pellentesque.</p>
+	<div class="my_milestones loading">
 
-	<h3>How do I sign up?</h3>
+	</div>
 
-	<p>Sed facilisis blandit ipsum, vitae tempor velit posuere vitae. Fusce vel nulla id neque aliquam elementum ut vel turpis. Aliquam non dui vehicula diam ultricies malesuada a in purus. Suspendisse quis nisl et diam blandit scelerisque. Mauris vehicula malesuada lorem, quis rhoncus justo elementum non. Integer sollicitudin porttitor erat eget congue. Mauris in nulla magna. Mauris at nisi nibh, ut tempus lectus. Maecenas vestibulum convallis nisi, sit amet lobortis tortor fermentum at. Aliquam a orci lacus. Duis accumsan vehicula leo, ut elementum nibh ultricies vitae.</p>
-
-	<pre>
-    	<?php print_r($content); ?>
-    </pre>
+	<a href="#" class="button">Add new Motivation</a>
 
 </div>
 
